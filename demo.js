589f6ad727e567c366ac2c1fead1770f48bf96df
shan <stan194@e.ntu.edu.sg>
2022-02-20 23:57:55 +0800

    Training

---
+++ demo.js
@@ -0,0 +1,27 @@
+document.write("javascript <br/>");
+document.write("hoho <br/>");
+
+var length=2, breadth=2, area;
+
+area=length*breadth;
+document.write(area);
+
+var num;
+num = 10;
+var Num = 20;
+var name = "Javascript";
+document.writeln(Num);
+document.writeln(name);
+
+var tBase=2, tHeight=3, tArea;
+var sLength=2, sArea;
+var value1=5, value2=2, value3=3,addValue,mulValue;
+tArea=0.5*tBase*tHeight;
+sArea=sLength*sLength;
+addValue=value1+value2+value3;
+mulValue=value1*value2*value3;
+
+document.writeln(tArea);
+document.writeln(sArea);
+document.writeln(addValue);
+document.writeln(mulValue);
